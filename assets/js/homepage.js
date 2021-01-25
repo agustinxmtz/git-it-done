0391229d1540773f561c9ae4305849a0a402657f
agustinxmtz <70080354+agustinxmtz@users.noreply.github.com>
2021-01-25 16:46:20 -0600

    added Error Handling to Repository requests

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -5,11 +5,20 @@ var getUserRepos = function(user) {
     //format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-    // make a request to the url
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data){
-            displayRepos(data, user);
-        });
+    fetch(apiUrl)
+        .then(function(response) {
+            // request was successful
+            if (response.ok) {
+                response.json().then(function(data) {
+                    displayRepos(data, user);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    })
+        .catch(function(error) {
+        // Notice this `.catch()` getting chained onto the end of the `.then()` method
+        alert("Unable to connect to GitHub");
     });
 };
 
@@ -32,6 +41,10 @@ var formSubmitHandler = function(event) {
 };
 
 var displayRepos = function(repos, searchTerm) {
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
     console.log(repos);
     console.log(searchTerm);
     repoContainerEl.textContent = "";
